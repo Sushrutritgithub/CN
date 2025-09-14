6ff0c5fdf4700f03d3dc1bc4a69cd6351fb981d0
Sushrut_Deshpande <152052113+Sushrutritgithub@users.noreply.github.com>
2025-09-15 01:13:52 +0530

    Update selective.c

--- selective.c
+++ selective.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define TOTAL_FRAMES 10
-#define WINDOW_SIZE 4
+#include <time.h>
 
 int main() {
-    int frame = 0;
-    int ack, i, received[WINDOW_SIZE];
+    int TOTAL_FRAMES, WINDOW_SIZE;
+    int frame = 0, i, received[50];
+
+    printf("Enter number of frames to send: ");
+    scanf("%d", &TOTAL_FRAMES);
+
+    printf("Enter window size: ");
+    scanf("%d", &WINDOW_SIZE);
+
+    srand(time(NULL));
 
     while (frame < TOTAL_FRAMES) {
-        // Send a window of frames
         printf("\nSender: Sending frames: ");
         for (i = 0; i < WINDOW_SIZE && (frame + i) < TOTAL_FRAMES; i++) {
             printf("%d ", frame + i);
-            received[i] = 0;  // initially unacknowledged
+            received[i] = 0;
         }
         printf("\n");
 
-        // Simulate ACK for each frame in the window
         for (i = 0; i < WINDOW_SIZE && (frame + i) < TOTAL_FRAMES; i++) {
-            printf("Receiver: ACK for frame %d? (1=Yes,0=No): ", frame + i);
-            scanf("%d", &ack);
+            int ack = rand() % 2;
             if (ack == 1) {
                 received[i] = 1;
-                printf("Sender: Frame %d acknowledged.\n", frame + i);
+                printf("Receiver: Frame %d received. ACK sent.\n", frame + i);
             } else {
-                printf("Sender: Frame %d lost! Will resend later.\n", frame + i);
+                printf("Receiver: Frame %d lost! No ACK.\n", frame + i);
             }
         }
 
-        // Slide window past acknowledged frames
         i = 0;
         while (i < WINDOW_SIZE && received[i] == 1) {
             frame++;
             i++;
         }
     }
+
     printf("\nAll frames sent successfully using Selective Repeat ARQ!\n");
     return 0;
 }
